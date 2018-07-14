1f36642b39f2ae376415ab1af81c4456dcc51754
marcosvbcosta <marcosvbcosta10@gmail.com>
2018-07-14 14:29:10 -0300

    modification in folders

---
+++ codes/register.js
@@ -0,0 +1,5 @@
+var name = document.getElementById('name');
+var cpf = document.getElementById('cpf');
+var email = document.getElementById('email');
+var birthDate = document.getElementById('birthDate');
+var phone = document.getElementById('phone');
